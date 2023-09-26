6eddeed95b3fa538d5be06da48d26510de35e94d
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-09-27 01:15:55 +0200

    a

--- alx-low_level_programming-master/all/all.c
+++ alx-low_level_programming-master/all/all.c
@@ -1,82 +1,29 @@
-#include"main.h"
+#include <stdio.h>
 
-/**
- * isASCII - print 32 / 126.
- * @a: int
- * Return: 1 / 0
- */
+int main(void){
+	int a, b, i = 0, j;
+	int sum = 0;
+	
 
-int isASCII(int a)
-{
-	return (a >= 32 && a <= 126);
-}
+	printf("Enter the number of elements in the array: ");
+	scanf("%d", &a);
+	
+	int arr[a];
 
-/**
- * printf1 - printf1(char *b, int s1, int e1)
- * @b: string
- * @s1: start
- * @e1: end
- */
-
-void printf1(char *b, int s1, int e1)
-{
-	int d = 0;
-
-	while (d < 10)
-	{
-		if (d < e1)
-			printf("%02x", *(b + s1 + d));
-		else
-			printf("  ");
-		if (d % 2)
-			printf(" ");
-		d++;
-	}
-}
-
-/**
- * printASCII -  prints a buffer.
- * @b: string
- * @s1: start
- * @e1: end
- */
-
-void printASCII(char *b, int s1, int e1)
-{
-	int c1, i = 0;
-
-	while (i < e1)
-	{
-		c1 = *(b + i + s1);
-		if (!isASCII(c1))
-			c1 = 46;
-		printf("%c", c1);
+	while (i < a){
+		printf("Enter element %d: ", i + 1);
+		scanf("%d", &b);
+		arr[i] = b;
 		i++;
 	}
-}
 
-/**
- * print_buffer -  prints a buffer.
- * @b: buffer
- * @size: size
- * Return: void
- */
-
-void print_buffer(char *b, int size)
-{
-	int s1, e1;
-
-	if (size > 0)
-	{
-		for (s1 = 0; s1 < size; s1 += 10)
-		{
-			e1 = (size - s1 < 10) ? size - s1 : 10;
-			printf("%08x: ", s1);
-			printf1(b, s1, e1);
-			printASCII(b, s1, e1);
-			printf("\n");
-		}
+	j = 0;
+	while (j < a){
+		sum += arr[j];
+		j++;
 	}
-	else
-		printf("\n");
+
+	printf("Sum of all elements: %d\n", sum);
+
+	return 0;
 }
--- alx-low_level_programming-master/all/tempCodeRunnerFile.c
+++ alx-low_level_programming-master/all/tempCodeRunnerFile.c
@@ -2,22 +2,19 @@
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int a, b, i = 0;
+	int sum;
+	int arr[a];
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	printf("Enter the number of elements in the array: \n");
+	scanf("%d", &a);
+	while (b <= a){
+		printf("Enter element: \n");
+		scanf("%d: %d", &b, &a);
+		arr[i] = b;
+		i++;
+	}
+	sum = 0;
+	printf("Sum of all elements: %d", sum);
+	return 0;
 }
