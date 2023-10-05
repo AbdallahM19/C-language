375cdbf9fefae7a9fb8b01530c51e0ea686c05d1
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-10-05 04:33:44 +0200

    malloc

--- My Work/alx-C-1.c
+++ My Work/alx-C-1.c
@@ -1,19 +1,77 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
-int main (void)
+/**
+ * w_count - to count the number of words in a string
+ * @s: string to evaluate
+ * Return: word
+ */
+int w_count(char *s)
 {
-	int num, a, b;
+	int len, a, b;
 
-	printf("Enter your number : ");
-	scanf("%d", &num);
+	len = 0;
+	a = 0;
 
-	for (a = 1; a <= num; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
-		
-		for (b = num; b >= a; b--)
+		if (s[a] == ' ')
+			len = 0;
+		else if (len == 0)
 		{
-			printf("%d", b);
+			len = 1;
+			b++;
 		}
-		printf("\n");
 	}
+
+	return (b);
+}
+
+/**
+ * **strtow - Write a function that splits a string into words.
+ * @str: string to split
+ *
+ * Return: pointer to an array
+ */
+
+char **strtow(char *str)
+{
+	char **tab2, *tab;
+	int i, k = 0, len = 0, words, c = 0, s, e;
+
+	while (*(str + len))
+		len++;
+	words = w_count(str);
+	if (words == 0)
+		return (NULL);
+
+	tab2 = (char **) malloc(sizeof(char *) * (words + 1));
+	if (tab2 == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				e = i;
+				tab = (char *) malloc(sizeof(char) * (c + 1));
+				if (tab == NULL)
+					return (NULL);
+				while (s < e)
+					*tab++ = str[s++];
+				*tab = '\0';
+				tab2[k] = tab - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			s = i;
+	}
+
+	tab2[k] = NULL;
+
+	return (tab2);
 }
--- My Work/alx-C.c
+++ My Work/alx-C.c
@@ -1,26 +1,75 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
-int main (void)
+/**
+ * w_count - Count the number of words in a string.
+ * @s: The input string.
+ * Return: The number of words.
+ */
+int w_count(char *s)
 {
-	int num, a, b, c = 1;
+	int n = 0, i;
 
-	printf("enter you number : ");
-	scanf("%d", &num);
-
-	for (a = 1; a = num; a++)
+	for (i = 0; s[i]; i++)
 	{
-		for (b = 1; b <= num; b++)
+		if (s[i] == ' ')
 		{
-			if (b > 0)
-			{
-				printf("%d", b);
-			}
-			else
-			{
-				break;
-			}
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				n++;
 		}
-		num--;
-		printf("\n");
+		else if (i == 0)
+			n++;
 	}
+	n++;
+	return (n);
+}
+
+/**
+ * strtow - Write a function that splits a string into words.
+ * @str: string.
+ * Return: A pointer to an array of strings (words).
+ */
+
+char **strtow(char *str)
+{
+	int a, j, b, l, n = 0, wc = 0;
+	char **word;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	wc = w_count(str);
+	if (n == 1)
+		return (NULL);
+	word = (char **)malloc(n * sizeof(char *));
+	if (word == NULL)
+		return (NULL);
+	word[n - 1] = NULL;
+	a = 0;
+	while (str[a])
+	{
+		if (str[a] != ' ' && (a == 0 || str[a - 1] == ' '))
+		{
+			for (j = 1; str[a + j] != ' ' && str[a + j]; j++)
+				;
+			j++;
+			word[wc] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (word[wc] == NULL)
+			{
+				for (b = 0; b < wc; b++)
+					free(word[b]);
+				free(word[n - 1]);
+				free(word);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				word[wc][l] = str[a + l];
+			word[wc][l] = '\0';
+			wc++;
+			a += j;
+		}
+		else
+			a++;
+	}
+	return (word);
 }
