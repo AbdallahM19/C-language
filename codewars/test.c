0b6da6ebfb6580a3d18a066037b27e956bf923d7
AbdallahM19 <abdallahmaabakar@gmail.com>
2023-11-25 20:10:28 +0200

    finish

--- codewars/test.c
+++ codewars/test.c
@@ -1,25 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-char *strrev (char *string)
+char* reverseWords(const char* text)
 {
-	char tab;
-	int i, j;
-	for (i = 0, j = strlen(string) - 1; i < j; i++, j--)
+	if (text == NULL)
+		return (NULL);
+	size_t len = strlen(text);
+	char *reversed = (char *)malloc((len + 1) * sizeof(char));
+	if (reversed == NULL)
+		return (NULL);
+	size_t i = 0, j = 0, start;
+	while (i < len)
 	{
-		tab = string[i];
-		string[i] = string[j];
-		string[j] = tab;
+		while (i < len && text[i] == ' ')
+		{
+			reversed[j++] = text[i++];
+		}
+		start = j;
+		while (i < len && text[i] != ' ')
+		{
+			reversed[j++] = text[i++];
+		}
+		for (size_t k = 0; k < (j - start) / 2; k++)
+		{
+			char temp = reversed[start + k];
+			reversed[start + k] = reversed[j - k - 1];
+			reversed[j - k - 1] = temp;
+		}
+		while (i < len && text[i] == ' ')
+		{
+			reversed[j++] = text[i++];
+		}
 	}
-	return (string); // reverse the string in place and return it
-}
-
-int main()
-{
-    char str[] = "world";
-    strrev(str);
-    printf("%s", str);
-    return 0;
+	reversed[j] = '\0';
+	return (reversed);
 }
